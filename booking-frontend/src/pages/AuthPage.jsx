cdb6fcf89a5453bef1399bec31847304c8176ed5
Delax314 <vasyakr18@gmail.com>
2026-09-10 22:40:57 +0200

    refactor(frontend): clean up authentication code

--- booking-frontend/src/pages/AuthPage.jsx
+++ booking-frontend/src/pages/AuthPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import { saveAuth } from "../utils/auth";
 
-export default function AuthPage({ onLogin }) {
+export default function AuthPage() {
   const [isLogin, setIsLogin] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
